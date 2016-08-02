a7196e44c9cf95fa48ba9043a3e57052408a57ee
Hardik Kaji <hardik.kaji@mastercard.com>
2016-08-02 16:57:47 +0530

    minor code vars fix;

--- src/server/modules/question/question.controller.js
+++ src/server/modules/question/question.controller.js
@@ -73,19 +73,19 @@ function getSerchResult(req, res) {
 function postFile(req, res) {
 	/** When using the "single"
 	 data come in "req.file" regardless of the attribute "name". **/
-	var tmp_path = req.file.path;
+	var temporaryPath = req.file.path;
 
 	/** The original name of the uploaded file
 	 stored in the variable "originalname". **/
-	var target_path = 'uploads/' + req.file.originalname;
+	var targetPath = 'uploads/' + req.file.originalname;
 
 	/** A better way to copy the uploaded file. **/
-	var src = fs.createReadStream(tmp_path);
-	var dest = fs.createWriteStream(target_path);
+	var src = fs.createReadStream(temporaryPath);
+	var dest = fs.createWriteStream(targetPath);
 	src.pipe(dest);
 	src.on('end', function() {
 		var model = null;
-		mongoXlsx.xlsx2MongoData(target_path, model, function(err, mongoData) {
+		mongoXlsx.xlsx2MongoData(targetPath, model, function(err, mongoData) {
 			_.find(mongoData, function(data, index) {
 				mongoData[index].tags = (data.tags).split(',');
 			});
