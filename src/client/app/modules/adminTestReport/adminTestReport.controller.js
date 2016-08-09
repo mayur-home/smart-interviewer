668e51fdc275575b08c443a9a0c3386e97b92c7b
Hardik Kaji <hardik.kaji@mastercard.com>
2016-08-09 15:10:33 +0530

    highchart intgrated; chart added for question answer report;

--- src/client/app/modules/adminTestReport/adminTestReport.controller.js
+++ src/client/app/modules/adminTestReport/adminTestReport.controller.js
@@ -15,7 +15,50 @@
 		//////////////
 
 		function activate() {
+			generatePieChart();
 			logger.info('Activated Test Report Controller Activated');
+		}
+
+		function generatePieChart() {
+			var result = _.countBy(testData, 'isCorrect');
+			$('#test-pie-chart-container').highcharts({
+				chart: {
+					plotBackgroundColor: null,
+					plotBorderWidth: null,
+					plotShadow: false,
+					type: 'pie'
+				},
+				title: {
+					text: 'Summary Test Result'
+				},
+				tooltip: {
+					pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+				},
+				plotOptions: {
+					pie: {
+						allowPointSelect: true,
+						cursor: 'pointer',
+						dataLabels: {
+							enabled: true,
+							format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+							style: {
+								color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+							}
+						}
+					}
+				},
+				series: [{
+					name: 'Test Results',
+					colorByPoint: true,
+					data: [{
+						name: 'Correct',
+						y: result.true
+					}, {
+						name: 'Wrong',
+						y: result.false
+					}]
+				}]
+			});
 		}
 
 		function markStatus(status, questionId, index) {
@@ -28,6 +71,7 @@
 			$http.post('api/userTest/setQuestionStatus', params)
 				.then(function(response) {
 					vm.testData[index].isCorrect = response.data.status;
+					generatePieChart();
 				});
 		}
 	}
