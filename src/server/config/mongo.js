90fdcd0000c89fa6efb05948f4b70ed4de0941f0
Patel, Mayur <Mayur.Patel@mastercard.com>
2016-08-12 19:00:39 +0530

    hello

--- src/server/config/mongo.js
+++ src/server/config/mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://172.24.132.139:27017/test');
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://mayur:mayur@ds153745.mlab.com:53745/smart-interviwer');
 
 module.exports = mongoose;
