423b67d0c882e3302441761f11ad85c7572a1ae8
Hardik Kaji <hardikkaji@gmail.com>
2016-08-27 16:08:15 +0530

    removed some changes;

--- gulpfile.js
+++ gulpfile.js
@@ -260,7 +260,7 @@ gulp.task('optimize', ['inject'], function() {
 		.pipe(jsAppFilter.restore())
 		// Get the vendor javascript
 		.pipe(jslibFilter)
-		// .pipe($.uglify()) // another option is to override wiredep to use min files
+		.pipe($.uglify()) // another option is to override wiredep to use min files
 		.pipe(jslibFilter.restore())
 		// Take inventory of the file names for future rev numbers
 		.pipe($.rev())
