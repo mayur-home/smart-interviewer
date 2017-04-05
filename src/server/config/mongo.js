1c860b9350857b6626df8dd22b26f4977b71a0b8
Hardik Kaji <hardik.kaji@mastercard.com>
2017-04-05 15:43:12 +0530

    fixed mailer issue;

--- src/server/config/mongo.js
+++ src/server/config/mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://172.24.132.139:27017/test');
-mongoose.connect('mongodb://mayur:mayur@ds019956.mlab.com:19956/smart-interviewer-new');
+mongoose.connect('mongodb://localhost:27017/test');
+// mongoose.connect('mongodb://mayur:mayur@ds019956.mlab.com:19956/smart-interviewer-new');
 
 module.exports = mongoose;
--- src/server/utils/mail.utils.js
+++ src/server/utils/mail.utils.js
@@ -1,9 +1,16 @@
 var nodemailer = require('nodemailer');
 var mailConfig = require('../config/mail.json');
 var base64 = require('base-64');
+var xoauth2 = require('xoauth2');
 
 function sendMail(from, to, subject, mailBody, isHTML) {
-	var transporter = nodemailer.createTransport(getTransporterUrl());
+	var transporter = nodemailer.createTransport({
+		service: "Gmail",
+		auth: {
+			xoauth2: xoauth2.createXOAuth2Generator(getTransportData())
+		}
+	});
+
 	var mailOptions = {
 		from: from,
 		to: to,
@@ -26,11 +33,13 @@ function sendMail(from, to, subject, mailBody, isHTML) {
 }
 
 
-function getTransporterUrl() {
-	return mailConfig.protocol + '://' +
-		mailConfig.mailId + ':' +
-		base64.decode(mailConfig.password) + '@' +
-		mailConfig.smtpServer;
+function getTransportData() {
+	return {
+		user: 'contact.smart.interviewer@gmail.com',
+		clientId: '937377912994-o7cfv59qgi4tcc018u4hjrumon9jkte8.apps.googleusercontent.com',
+		clientSecret: '5cmFFxIpLmrtVmAFGjHwj39b',
+		refreshToken: '1/kh8qotjsplV8-QAFBk-DKE_G1-MCebANBAScqS9c_Z8'
+	}
 }
 
 module.exports = {
